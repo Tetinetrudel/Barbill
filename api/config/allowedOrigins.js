e942fb69bce2495d5f79c2f9c12292f0bdfed3a2
Etienne Trudel <etrudel@gmail.com>
2023-11-14 16:06:00 -0500

    Connecting database

---
+++ api/config/allowedOrigins.js
@@ -0,0 +1,5 @@
+export const allowedOrigins = [
+    'https://barbill.onrender.com',
+    'http://127.0.0.1:5173',
+    'http://localhost:5173'
+]
--- api/server.js
+++ api/server.js
@@ -1,5 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+import { corsOptions } from './config/corsOptions.js';
+import { connectDB } from './config/connectDb.js';
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
-app.listen(5000, () => { console.log("App running on port 5000")})
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
+})
